edfe7e61545cdf11b04f01a987c9987b1ce38603
Katie Wilkinson <ktwilkinson12@gmail.com>
2016-11-05 12:14:02 -0700

    first commit

---
+++ oddeven.py
@@ -0,0 +1,9 @@
+# counts from 1 to 2000
+for a in range (1, 20001):
+    #generates whether the number is odd or even
+    if a % 2 == 0:
+    #if number is odd print this
+  print "Number is", + str(i) + ". This is an odd number."
+else:
+    #otherwise print this for even numbers
+    print "Number is" + str(i) + ". This is an even number."
